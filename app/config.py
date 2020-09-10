a14ec854f669261281ae98be68695837e0d2daf6
emanuelosva <emanuelosva@gmail.com>
2020-09-09 21:11:33 -0500

    Add dotenv multiplataform support

--- app/config.py
+++ app/config.py
@@ -2,8 +2,15 @@
 Unu API - Application settings.
 """
 
+import os
+
 from typing import List
 from pydantic import BaseSettings
+
+from dotenv import load_dotenv
+
+# Get env variables from dotenv files for development
+load_dotenv()
 
 
 class Settings(BaseSettings):
@@ -23,45 +30,46 @@ class Settings(BaseSettings):
 
     APP_NAME: str = "Unu - API"
     API_V1_STR: str = "/api/v1"
-    CORS_ORIGIN: List[str]
+    CORS_ORIGIN: List[str] = ["*"]
     EMAIL_ADMIN: str = "debuggers.master@gmail.com"
-    API_URL: str
+    API_URL: str = os.getenv("API_URL")
 
     ###########################################
     ##                Security               ##
     ###########################################
 
-    SECRET_JWT: str = "secret"
+    SECRET_JWT: str = os.getenv("SECRET_JWT")
 
     ###########################################
     ##               DataBase                ##
     ###########################################
 
-    DB_NAME: str
-    DB_PASSWORD: str
-    DB_CLUSTER: str
-    DB_USERNAME: str
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_CLUSTER: str = os.getenv("DB_CLUSTER")
+    DB_USERNAME: str = os.getenv("DB_USERNAME")
 
     ###########################################
     ##         Email Configurations          ##
     ###########################################
-    SENDGRID_API_KEY: str
+    SENDGRID_API_KEY: str = os.getenv("SENDGRID_API_KEY")
     EMAIL_SENDER: str = "unu.events@gmail.com"
 
     ###########################################
     ##               Task Queue              ##
     ###########################################
 
-    REDIS_URL: str
-    QUEUES: List[str]
+    REDIS_URL: str = os.getenv("REDIS_URL")
+    QUEUES: List[str] = ["email", "default"]
 
     ###########################################
     ##          External Storage             ##
     ###########################################
 
-    GOOGLE_STORAGE_BUCKET: str
-    ALLOWED_EXTENSIONS: List[str]
-    GOOGLE_APPLICATION_CREDENTIALS: str
+    GOOGLE_STORAGE_BUCKET: str = os.getenv("GOOGLE_STORAGE_BUCKET")
+    ALLOWED_EXTENSIONS: List[str] = ["png", "jpg", "jpeg"]
+    GOOGLE_APPLICATION_CREDENTIALS: str = os.getenv(
+        "GOOGLE_APPLICATION_CREDENTIALS")
 
 
 settings = Settings()
